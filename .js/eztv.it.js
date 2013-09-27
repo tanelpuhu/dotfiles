35a40669a421a9ce8b9a31b44a65da81413b59f1
Tanel Puhu <tanel@tanelpuhu.com>
2013-09-27 08:48:12 +0300

    nomore Z subl less -X .js link

--- .js/eztv.it.js
+++
@@ -1,7 +0,0 @@
-$('#header_logo').remove();
-if('/' === location.pathname) {
-  $('.forum_header_border:nth(2)').remove();
-  $('.forum_header_border:nth(2)').remove();
-  $('.forum_header_border:nth(2)').remove();
-  $('br').remove();
-}
